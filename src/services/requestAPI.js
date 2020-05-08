af6c666235c82cadc98319be7980bd5656d3f3a0
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-05-08 01:12:59 -0300

    Bugfix: Adjust API URL with netlify and dev enviroment

--- src/services/requestAPI.js
+++ src/services/requestAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "https://search-cnpj.herokuapp.com",
+	baseURL: "https://search-cnpj.herokuapp.com" || process.env.REACT_APP_API_URL,
 });
 
 export default api;
