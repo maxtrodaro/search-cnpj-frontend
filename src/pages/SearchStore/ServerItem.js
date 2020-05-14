704bad3b425d9f03b15c0567a69047b838026be9
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-05-13 21:19:26 -0300

    Feature: Implementing how many stores on server

--- src/pages/SearchStore/ServerItem.js
+++ src/pages/SearchStore/ServerItem.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "../../services/requestAPI";
 
 export default function ServerItem({ server }) {
+	const [qtd, setQtd] = useState([]);
+
+	useEffect(() => {
+		const loadQtdStores = async (ip) => {
+			const response = await api.get(`/store/${ip}`);
+
+			setQtd(response.data.count);
+		};
+
+		loadQtdStores(server.ip);
+	}, [server]);
+
 	return (
 		<li className="search-container__servs__bottom__list__item">
 			<p className="search-container__servs__bottom__list__item__name">
@@ -9,7 +23,7 @@ export default function ServerItem({ server }) {
 			<p className="search-container__servs__bottom__list__item__serv">
 				{server.ip}
 			</p>
-			{/* <p className="search-container__servs__bottom__list__item__qtd">11</p> */}
+			<p className="search-container__servs__bottom__list__item__qtd">{qtd}</p>
 		</li>
 	);
 }
