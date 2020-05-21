18cd42ead166b1706aaea39b8b21228a261f4ed5
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-05-21 20:20:52 -0300

    Adding console.log

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -22,6 +22,7 @@ export default function Login() {
 	const history = useHistory();
 
 	const handleSubmit = async (values) => {
+		console.log(values);
 		try {
 			const response = await api.post("/authenticate", values);
 
