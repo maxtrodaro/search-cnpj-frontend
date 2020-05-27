847080f6d6f9956fcaedde5e028026d3c294dbc9
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-05-26 21:08:12 -0300

    Feature: Implementing hexadecimal variables

--- src/util/Style/global.js
+++ src/util/Style/global.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import variables from "./variables";
 
 export const Input = styled.input`
 	width: 350px;
 	height: 60px;
-	color: #707070;
-	border: 2px solid #e0e0e0;
+	color: ${variables.colors.gray};
+	border: 2px solid ${variables.colors.borderGray};
 	border-radius: 6px;
 	padding: 0 50px;
 	margin-top: 20px;
@@ -20,22 +21,30 @@ export const Button = styled.button`
 	line-height: 60px;
 	font-weight: 600;
 	width: 350px;
-	background: #3a0d51;
-	background: -webkit-linear-gradient(to right, #3a0d51, #621d3d);
-	background: linear-gradient(to right, #3a0d51, #621d3d);
-	color: #fff;
+	background: ${variables.colors.primaryColor};
+	background: -webkit-linear-gradient(
+		to right,
+		${variables.colors.primaryColor},
+		${variables.colors.linearColor}
+	);
+	background: linear-gradient(
+		to right,
+		${variables.colors.primaryColor},
+		${variables.colors.linearColor}
+	);
+	color: ${variables.colors.white};
 	&:disabled {
-		background: #e0e0e0;
-		color: #757575;
+		background: ${variables.colors.borderGray};
+		color: ${variables.colors.darkGray};
 	}
 `;
 
 export const HomeButton = styled.button`
 	height: 60px;
-	background: #f57c00;
+	background: ${variables.colors.secundaryColor};
 	border: 0;
 	border-radius: 0 0 6px 6px;
-	color: #ffffff;
+	color: ${variables.colors.white};
 	font-weight: 700;
 	text-align: center;
 	text-decoration: none;
@@ -46,10 +55,10 @@ export const HomeButton = styled.button`
 
 export const RegisterButton = styled.button`
 	height: 60px;
-	background: #f57c00;
+	background: ${variables.colors.secundaryColor};
 	border: 0;
 	border-radius: 6px;
-	color: #f5f5f5;
+	color: ${variables.colors.lightGray};
 	text-align: center;
 	text-decoration: none;
 	line-height: 60px;
@@ -59,8 +68,8 @@ export const RegisterButton = styled.button`
 		margin-right: 20px;
 	}
 	&:disabled {
-		background: #e0e0e0;
-		color: #757575;
+		background: ${variables.colors.borderGray};
+		color: ${variables.colors.darkGray};
 	}
 `;
 
@@ -73,8 +82,8 @@ export const RegisterLink = styled.a`
 export const InputSearch = styled.input`
 	width: 180px;
 	height: 60px;
-	color: #333333;
-	border: 2px solid #e0e0e0;
+	color: ${variables.colors.lightBlack};
+	border: 2px solid ${variables.colors.borderGray};
 	border-radius: 6px 0 0 6px;
 	padding: 0 10px;
 `;
@@ -83,7 +92,7 @@ export const ButtonSearch = styled.button`
 	height: 60px;
 	padding: 5px;
 	border-radius: 0 6px 6px 0;
-	border: 2px solid #f57c00;
-	background: #f57c00;
+	border: 2px solid ${variables.colors.secundaryColor};
+	background: ${variables.colors.secundaryColor};
 	margin-top: 0;
 `;
---
+++ src/util/Style/variables.js
@@ -0,0 +1,17 @@
+export default {
+	colors: {
+		white: "#FFFFFF",
+		backgroundWhite: "#FAFAFA",
+		black: "#000000",
+		lightBlack: "#333333",
+		gray: "#707070",
+		lightGray: "#F5F5F5",
+		mediumGray: "#CCCCCC",
+		darkGray: "#757575",
+		borderGray: "#E0E0E0",
+		red: "#F44336",
+		primaryColor: "#3A0D51",
+		linearColor: "#621D3D",
+		secundaryColor: "#F57C00",
+	},
+};
